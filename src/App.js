163cce3a5896a6670e7b7b63c781c2fdc05fa752
Mayank Saraswat <mayanksaraswat497@gmail.com>
2023-02-28 18:52:18 +0530

    components deleted

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
-// import MyFunc from "./Functions";
-// import MyClass from "./Classes";
 import './App.css';
-import AxiosCall from "./AxiosCall";
+import AxiosCall from "./Components/AxiosCall";
 
 function App() {
   
   return (
     <div className="App">
-      
-      {/* < MyFunc/>
-      < MyClass/> */}
+    
       <AxiosCall/>
     
     </div>
